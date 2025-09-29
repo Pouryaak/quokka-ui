5efe1c4bd5f4160d6c4280f10f335552f06c29f5
Pouryaak <pourya.ak1996@gmail.com>
2025-09-30 00:44:45 +0200

    chore: setup project

---
+++ .storybook/preview.ts
@@ -0,0 +1,25 @@
+import type { Preview } from "@storybook/react-vite";
+import { withThemeByDataAttribute } from "@storybook/addon-themes";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    withThemeByDataAttribute({
+      themes: {
+        light: "light",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+      attributeName: "data-theme",
+    }),
+  ],
+};
+
+export default preview;
