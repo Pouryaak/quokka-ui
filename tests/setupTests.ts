0396973750b497a0537c0dc9b114e9c788fa5e77
Pouryaak <pourya.ak1996@gmail.com>
2025-10-16 22:43:48 +0200

    feat: update setup test

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -1,7 +1,6 @@
-import { expect, afterEach } from "vitest";
-import matchers from "@testing-library/jest-dom/matchers";
+import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
 import "vitest-axe/extend-expect";
 
-expect.extend(matchers);
 afterEach(() => cleanup());
